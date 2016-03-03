a355950c207a22f795db8bf474f92a1ab33ead97
Daniel Mazzer <dmazzer@gmail.com>
2016-03-03 10:48:08 -0300

    merge changes from develop

--- remote/src/nors.py
+++ remote/src/nors.py
@@ -1,24 +1,42 @@
-import time
-#import grovepi
+#!/usr/bin/env python2
 
+""" 
+nors.py: Noticia Remote Management and Supervisor
 
-from norsutils.logmsgs.logger import Logger
+"""
 
-logger = Logger()
-logger.log("SIS application started.")
+__author__ = "Daniel Mazzer"
+__copyright__ = "Copyright 2016, NORS project"
+__credits__ = ""
+__license__ = "GPL"
+__maintainer__ = "Daniel Mazzer"
+__email__ = "dmazzer@gmail.com"
 
+# import sys
+import signal
+from sensorservice.sensorservice import Nors_SensorService
+from genericsensor.genericsensor import Nors_GenericSensor
+from sensors import sensor_dht
 
+if __name__ == '__main__':
+    
+    from norsutils.logmsgs.logger import Logger
 
-# Connect the Grove Tilt Switch to digital port D3
-# SIG,NC,VCC,GND
-# mag_switch = 3
-# 
-# grovepi.pinMode(mag_switch,"INPUT")
-# 
-# while True:
-#     try:
-#         print grovepi.digitalRead(mag_switch)
-#         time.sleep(.5)
-# 
-#     except IOError:
-#         print "Error"
+    logger = Logger()
+    logger.log("NORS - Noticia Remote Management and Supervisor")
+    sensor_service = Nors_SensorService()
+    sensor_generic1 = Nors_GenericSensor(gs_name='fake1', gs_pull_interval=2, gs_read_fisical_interval=1)
+    sensor_generic1.SignIn()
+    sensor_generic2 = Nors_GenericSensor(gs_name='fake2', gs_pull_interval=2, gs_read_fisical_interval=1)
+    sensor_generic2.SignIn()
+    
+    sensor_dht = sensor_dht.RealSensor()
+
+    
+    def do_exit(sig, stack):
+        raise SystemExit('Exiting')
+     
+    signal.signal(signal.SIGINT, do_exit)
+    signal.signal(signal.SIGUSR1, do_exit)
+     
+    signal.pause()    
