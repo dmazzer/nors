232c7d5c058012ae1b0fbdea231b86f8a0e8e043
Daniel Mazzer <dmazzer@gmail.com>
2016-05-02 21:36:07 -0300

    added connect class (under development)

---
+++ remote/src/client/connect.py
@@ -0,0 +1,38 @@
+""" 
+connect.py: Authenticate and register on remote server   
+
+"""
+__author__ = "Daniel Mazzer"
+__copyright__ = "Copyright 2016, NORS Project"
+__credits__ = ""
+__license__ = "MIT"
+__maintainer__ = "Daniel Mazzer"
+__email__ = "dmazzer@gmail.com"
+
+
+from norsutils.logmsgs.logger import Logger
+logger = Logger('debug')
+
+import requests
+
+class Nors_Connect():
+    def __init__(self, server_ip, server_port, client_auth, client_id):
+        
+        logger.log('Sending GET command to server:', 'debug')
+        r = requests.get('http://127.0.0.1:8080/v1/server/info')
+        logger.log('Response:', 'debug')
+        logger.log(r.text, 'debug')
+        logger.log('Headers:', 'debug')
+        logger.log(r.headers, 'debug')
+        
+        pass
+    
+    def sign_in(self):
+        pass
+    
+    def auth(self):
+        pass
+        
+        
+
+
