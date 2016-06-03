28b6009a992f7907435d42074246ab8e81a742d2
Daniel Mazzer <dmazzer@gmail.com>
2016-06-03 12:55:25 -0300

    adding rest support, work in progress

--- server/src/controllers/clients_controller.py
+++ server/src/controllers/clients_controller.py
@@ -1,3 +1,20 @@
+""" 
+clients_controller.py:  Restful resources - Clients context
+
+"""
+
+__author__ = "Daniel Mazzer"
+__copyright__ = "Copyright 2016, NORS Project"
+__credits__ = ""
+__license__ = "MIT"
+__maintainer__ = "Daniel Mazzer"
+__email__ = "dmazzer@gmail.com"
+
+import sys
+
+sys.path.append('../../../remote/src/')
+from norsutils.logmsgs.logger import Logger
+logger = Logger('info')
 
 def clients_client_id_get(client_id, limit) -> str:
     return ('Client_ID: ' + str(client_id))
